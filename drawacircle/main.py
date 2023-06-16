2de66705efd7b3eec186b26122e12da34c5ade40
Tony <3261429104@qq.com>
2023-06-16 19:31:19 +0800

    use where function

--- drawacircle/main.py
+++ drawacircle/main.py
@@ -1,4 +1,10 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
-arr1 = np.random.randn(5, 3)
+arr1 = np.random.randn(8, 3)
+print(arr1)
+
+arr2 = np.where(arr1 > 0, 1, -1) # if arr1 > 0, then 1, else -1
+print(arr2)
+
+arr3 = arr1 > 0
+print(arr3)
